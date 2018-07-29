128929fc41cebc096e762fd9efd7b79143ce31dc
Shannon Holmes <snholmes13@gmail.com>
2018-07-29 19:42:27 +0100

    add test bar has rooms

--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -1,15 +1,24 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../bar.rb")
+require_relative("../rooms.rb")
 
 class BarTest < MiniTest::Test
 
   def setup()
-    @bar = Bar.new("CodeClan Caraoke")
+    @room1 = Rooms.new("One")
+    @room2 = Rooms.new("Two")
+    @rooms = [@room1, @room2]
+
+    @bar = Bar.new("CodeClan Caraoke", @rooms)
   end
 
   def test_bar_has_name()
     assert_equal("CodeClan Caraoke", @bar.name())
+  end
+
+  def test_bar_has_rooms()
+    assert_equal(2, @bar.rooms().count())
   end
 
 end
