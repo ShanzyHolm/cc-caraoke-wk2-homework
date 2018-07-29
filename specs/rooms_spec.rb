ecf1d1de24f232eaf0d3551e54877b9ca04cf87c
Shannon Holmes <snholmes13@gmail.com>
2018-07-29 18:52:32 +0100

    initial commit - add room name test

---
+++ specs/rooms_spec.rb
@@ -0,0 +1,15 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../rooms.rb")
+
+class RoomsTest < MiniTest::Test
+
+  def setup()
+    @room = Rooms.new("One")
+  end
+
+  def test_room_has_name()
+    assert_equal("One", @room.name())
+  end
+
+end
